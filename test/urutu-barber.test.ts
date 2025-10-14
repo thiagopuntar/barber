b8285e1a27d5861ffa971c8b5806ed16e66b5ab5
(Admin) Thiago Puntar <admin.thiago.puntar@macbookpro.lan>
2025-10-14 14:26:38 -0300

    Initial commit

---
+++ test/urutu-barber.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as UrutuBarber from '../lib/urutu-barber-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/urutu-barber-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new UrutuBarber.UrutuBarberStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
